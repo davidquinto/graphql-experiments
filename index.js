f815697cd1ba29e288c4d994f838a74c61db5843
davidquinto <davidsantiagoquinto@gmail.com>
2018-10-30 14:47:44 -0500

    using grapqhl objects

--- index.js
+++ index.js
@@ -1,79 +1,58 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var graphql = require('graphql');
 
-var schema = buildSchema(`
-  type Query {
-    getHuman(id: ID!): Client,
-    clients: [Client]
-  }
-  type Client {
-    id: ID!,
-    name: String,
-    age: Int,
-    hobbies: [Hobby]
-  }
-  type Hobby {
-    name: String,
-    level: HobbyLevel 
-  }
-  enum HobbyLevel {
-    NOVICE,
-    INTERMEDIATE,
-    EXPERT
-  }
-`);
+var data = require('./data.json');
 
-var fakeDatabase = [
-  {
-    id: 1,
-    name: 'Hector',
-    age: 22,
-    hobbies: [
-      {
-        name: 'Videogames',
-        level: 'INTERMEDIATE'
-      }
-    ]
-  },
-  {
-    id: 2,
-    name: 'Susana',
-    age: 26,
-    hobbies: [
-      {
-        name: 'Football',
-        level: 'INTERMEDIATE'
-      }
-    ]
-  },
-  {
-    id: 3,
-    name: 'Andy',
-    age: 32,
-    hobbies: [
-      {
-        name: 'Reading',
-        level: 'EXPERT'
-      }
-    ]
+var hobbyLevelType = new graphql.GraphQLEnumType({
+  name: 'HobbyLevel',
+  values: {
+    NOVICE: { value: 0 },
+    INTERMEDIATE: { value: 1 },
+    EXPERT: { value: 2 }
   }
-]
+});
 
-var root = {
-  getHuman: function({ id }) {
-    if (!fakeDatabase[id]) {
-      throw new Error('no human exists with id ' + id);
+var hobbyType = new graphql.GraphQLObjectType({
+  name: 'Hobby',
+  fields: {
+    name: { type: graphql.GraphQLString },
+    level: { type: hobbyLevelType },
+  }
+});
+
+var clientType = new graphql.GraphQLObjectType({
+  name: 'Client',
+  fields: {
+    id: { type: graphql.GraphQLID },
+    name: { type: graphql.GraphQLString },
+    age: { type: graphql.GraphQLInt },
+    hobbies: { type: new graphql.GraphQLList(hobbyType) },
+  }
+});
+
+// Define the Query type
+var queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    getClient: {
+      type: clientType,
+      // `args` describes the arguments that the `user` query accepts
+      args: {
+        id: { type: graphql.GraphQLID }
+      },
+      resolve: function (_, { id }) {
+        return data.clients[id];
+      }
     }
-    return fakeDatabase[id]
-  },
-  clients: fakeDatabase
-};
+  }
+});
+
+var schema = new graphql.GraphQLSchema({query: queryType});
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true,
 }));
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
